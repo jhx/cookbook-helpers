0ad30f429f871e90205ab5c0d588583100b8be96
Doc Walker <doc.walker@jameshardie.com>
2013-01-26 22:53:19 -0600

    Add name metadata

--- metadata.rb
+++ metadata.rb
@@ -3,4 +3,5 @@ maintainer_email "doc.walker@jameshardie.com"
 license          "Apache 2.0"
 description      "Installs/Configures helpers"
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          "0.1.0"
+version          "0.1.1"
+name              'helpers'
