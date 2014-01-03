2165f3d1714ded588484096cafd954ce35897751
Doc Walker <doc.walker@jameshardie.com>
2014-01-03 15:34:01 -0600

    Fix #2 update default secret_file

--- libraries/decrypt_data_bag.rb
+++ libraries/decrypt_data_bag.rb
@@ -7,9 +7,9 @@
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
 # You may obtain a copy of the License at
-# 
+#
 #     http://www.apache.org/licenses/LICENSE-2.0
-# 
+#
 # Unless required by applicable law or agreed to in writing, software
 # distributed under the License is distributed on an "AS IS" BASIS,
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -20,7 +20,7 @@
 class Chef
   class Recipe
     def decrypt_data_bag(bag, item = node.chef_environment,
-        secret_file = Chef::EncryptedDataBagItem::DEFAULT_SECRET_FILE)
+        secret_file = Chef::Config[:encrypted_data_bag_secret])
       Chef::Log.info self.class
       Chef::DataBag.validate_name!(bag.to_s)
       Chef::DataBagItem.validate_id!(item)
--- metadata.rb
+++ metadata.rb
@@ -3,5 +3,5 @@ maintainer_email "doc.walker@jameshardie.com"
 license          "Apache 2.0"
 description      "Installs/Configures helpers"
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          "0.1.2"
+version          "0.1.3"
 name              'helpers'
